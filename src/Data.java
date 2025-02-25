f743df2942da99f6241d841e1be24b6c49c3e9e3
giorgizena123 <zenaishvili621@gmail.com>
2025-02-25 23:58:49 +0400

    Update Data.java

--- src/Data.java
+++ src/Data.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+class Data {
+    private static final String FILE_NAME = "contacts.ser";
 
-public class Data {
+    public void serializeContacts(ArrayList<PhoneContact> contacts) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(contacts);
+        }
+    }
+
+    public ArrayList<PhoneContact> deserializeContacts() throws IOException, ClassNotFoundException {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            return (ArrayList<PhoneContact>) ois.readObject();
+        }
+    }
 }
